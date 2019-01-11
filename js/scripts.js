d16ef167e80bca4e37f21e254bb70b76e80c13b4
Hamzah Lorgat <hamzah_97@hotmail.com>
2019-01-11 11:13:04 +0000

    Add a menu toggle to show and hide the menu on mobile devices to improve user experience

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('Javascript Works.')
+const navToggle = document.getElementById('navbar-toggle');
+const nav = document.getElementById('nav-menu');
+
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('active')
+  console.log('burger clicked');
+});
+
+console.log('Javascript Works.');
