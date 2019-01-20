d09900c94d06fa46152ad8a51aebe91a54c146a5
Hamzah Lorgat <hamzah_97@hotmail.com>
2019-01-20 01:13:24 +0000

    Remove console.log references

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,5 @@ const nav = document.getElementById('nav-menu');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('active')
-
   navToggle.classList.toggle('toggle');
-  console.log('burger clicked');
 });
-
-console.log('Javascript Works.');
