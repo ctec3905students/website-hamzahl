c74f6714c3ed6db25d3bae9c2b89730884755cda
Hamzah Lorgat <hamzah_97@hotmail.com>
2019-01-20 00:44:16 +0000

    Add js to dynamically to visualise the skill progress

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@
 
 const navToggle = document.getElementById('navbar-toggle');
 const nav = document.getElementById('nav-menu');
+const htmlProgressBar = document.getElementById('progress-html');
+const phpProgressBar = document.getElementById('progress-php')
+
+const skillProgressBars = document.getElementsByTagName('progress');
+const testBtn = document.getElementById('test');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('active')
@@ -9,5 +14,16 @@ navToggle.addEventListener('click', () => {
   navToggle.classList.toggle('toggle');
   console.log('burger clicked');
 });
+
+// Sets the width and background for the skill bars
+const setSkillBar = () => {
+  [...document.querySelectorAll('.skill-bar')].forEach((e) => {
+    // Set inner div to the % specified in span text
+    e.children[0].style.width = e.children[0].children[0].innerText;
+    e.children[0].style.backgroundColor = 'red';
+  });
+} 
+
+setSkillBar();
 
 console.log('Javascript Works.');
