d4f41f7033468d531695564c37dfee3077506b09
Hamzah Lorgat <hamzah_97@hotmail.com>
2019-01-20 15:16:44 +0000

    Add validation to form to prevent user form entering wrong input

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 'use strict';
 
+// Element IDs
 const navToggle = document.getElementById('navbar-toggle');
 const nav = document.getElementById('nav-menu');
-const btnScrollTop = document.getElementById('scroll-top'); 
+const btnScrollTop = document.getElementById('scroll-top');
+const userName = document.getElementById('user-name');
+const userEmail = document.getElementById('user-email');
+const userMessage = document.getElementById('user-message');
+const contactForm = document.getElementById('contact-form');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('active')
@@ -28,6 +33,57 @@ const setSkillBar = () => {
     e.children[0].style.width = e.children[0].children[0].innerText;
     e.children[0].style.backgroundColor = 'red';
   });
-} 
+}
+
+
+// Validation methods for form
+const isEmpty = (inputField) => {
+  return (inputField.value === '' ? true : false);
+}
+
+const isValidName = (nameField) => {
+  const re = /^[a-z ,.'-]+$/i;
+  return re.test(nameField.value);
+}
+
+const isValidEmail = (emailField) => {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(emailField.value);
+}
+
+const invalidateInput = (message, field) => {
+  alert(message);
+  field.value = '';
+  field.focus();
+}
+
+// else clauses aren't needed for this function
+const validateForm = () => {
+  if (isEmpty(userName)) {
+    invalidateInput('Please enter a name', userName);
+  } else if (!isValidName(userName)) {
+    invalidateInput('Named not valid. Try again.', userName);
+  }
+
+  if (isEmpty(userEmail)) {
+    invalidateInput('Please enter an email', userEmail)
+  } else if (!isValidEmail(userEmail)) {
+    invalidateInput('Email not valid. Try again.', userEmail);
+  }
+
+  if (isEmpty(userMessage)) {
+    invalidateInput('Please enter a message', userMessage);
+  }
+}
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validateForm();
+  if (isEmpty(userName) || isEmpty(userEmail) || isEmpty(userEmail)) {
+    alert('Form not submitted: Input fields are empty.');
+  } else {
+    alert('Form Submitted.');
+  }
+});
 
 setSkillBar();
